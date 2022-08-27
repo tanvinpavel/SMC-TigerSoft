6bbf8edf5c0b75137c915aeb8e378f1a947be5af
Rakib Hossain <rakibjp431@gmail.com>
2022-08-27 14:17:02 -0700

    added text

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div>
             <h1>this is footer</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste dignissimos rem eos rerum at mollitia perspiciatis fuga odit nostrum quaerat!</p>
             
         </div>
     );
